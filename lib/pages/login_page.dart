aae3c4a650afd8338b1c214247662870dcab7f77
khaira43 <rkhaira1010@gmail.com>
2024-07-15 20:05:04 -0400

    Voila

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,50 @@
+import 'package:chat/components/my_textfield.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _pwController = TextEditingController();
+  LoginPage({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.message,
+              size: 60,
+              color: Theme.of(context).colorScheme.primary,
+           ),
+           const SizedBox(height: 50),
+           Text(
+            "Welcome back, you've been missed",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+
+            ),
+           ),
+           const SizedBox(height: 25),
+           MyTextField(
+            hintText: "Email",
+            obscureText: false,
+            controller: _emailController,
+           ),
+           const SizedBox(height: 10),
+           MyTextField(
+            hintText: "Password",
+            obscureText: true,
+            controller: _pwController,
+           ),
+           const SizedBox(height: 25),
+
+          ],
+        ),
+      ),
+    );
+  }
+}
