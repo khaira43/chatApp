8fab3e50b83eaab6069cdc0cbd047b1194a2c196
khaira43 <rkhaira1010@gmail.com>
2024-07-17 17:35:14 -0400

    .

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat/components/my_button.dart';
 import 'package:chat/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
@@ -5,6 +6,9 @@ class LoginPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _pwController = TextEditingController();
   LoginPage({super.key});
+  void login(){
+
+  }
   
   @override
   Widget build(BuildContext context) {
@@ -41,6 +45,29 @@ class LoginPage extends StatelessWidget {
             controller: _pwController,
            ),
            const SizedBox(height: 25),
+
+           MyButton(
+            text: "Login",
+            onTap: login,
+           ),
+
+           const SizedBox(height: 25),
+
+           Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Not a member? "),
+              Text("Register now", style: TextStyle(
+                fontWeight: FontWeight.bold
+                ),
+              ),
+
+            ],
+
+           ),
+
+          
+           
 
           ],
         ),
